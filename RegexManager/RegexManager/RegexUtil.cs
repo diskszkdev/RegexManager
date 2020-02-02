eb61989d93ffa87bd37d13ac85a8eb7fff86991b
diskszkdev <diskszkdev@gmail.com>
2020-02-02 15:39:59 +0900

    new project

---
+++ RegexManager/RegexManager/RegexUtil.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexManager
+{
+    public class RegexUtil
+    {
+        /// <summary>
+        /// 半角数字
+        /// </summary>
+        private const string HALF_NUMBER = @"^[0-9]+$";
+
+        /// <summary>
+        /// 半角英字
+        /// </summary>
+        private const string HALF_ALPHABET = @"^[a-zA-Z]+$";
+
+
+        public bool IsHalfNumber(string input)
+        {
+            var result = Regex.IsMatch(input, HALF_NUMBER);
+            return result;
+        }
+
+        public bool IsHalfAlphabet(string input)
+        {
+            var result = Regex.IsMatch(input, HALF_ALPHABET);
+            return result;
+        }
+    }
+}
---
+++ RegexManager/RegexManagerTest/RegexUtilTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegexManager;
+using System;
+
+namespace RegexManagerTest
+{
+    [TestClass]
+    public class RegexUtilTest
+    {
+        private RegexUtil _regexUtil;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _regexUtil = new RegexUtil();
+        }
+
+        [TestMethod]
+        public void HalfNumber_OK()
+        {
+            var data = "0123456789";
+            var result = _regexUtil.IsHalfNumber(data);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HalfNumber_NG()
+        {
+            var data = "0123456789m";
+            var result = _regexUtil.IsHalfNumber(data);
+            Assert.IsFalse(result);
+
+            data = "";
+            result = _regexUtil.IsHalfNumber(data);
+            Assert.IsFalse(result);
+
+            Assert.ThrowsException<ArgumentNullException>(() => _regexUtil.IsHalfNumber(null));
+        }
+
+        [TestMethod]
+        public void HalfAlphabet_OK()
+        {
+            var data = "abcdefghijklmnopqrstuvwxyz";
+            var result = _regexUtil.IsHalfAlphabet(data);
+            Assert.IsTrue(result);
+
+            data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            result = _regexUtil.IsHalfAlphabet(data);
+            Assert.IsTrue(result);
+
+            data = "abcdefghijklMNOPQRSTUVWXYZ";
+            result = _regexUtil.IsHalfAlphabet(data);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HalfAlphabet_NG()
+        {
+            var data = "abcdefghijklmnopqrstuvwxyz5";
+            var result = _regexUtil.IsHalfAlphabet(data);
+            Assert.IsFalse(result);
+
+            data = "";
+            result = _regexUtil.IsHalfAlphabet(data);
+            Assert.IsFalse(result);
+
+            Assert.ThrowsException<ArgumentNullException>(() => _regexUtil.IsHalfAlphabet(null));
+        }
+    }
+}
